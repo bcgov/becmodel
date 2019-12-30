96f2cb2821a76e77a738e1e716e8be963705c3c3
Simon Norris <snorris@hillcrestgeo.ca>
2019-12-30 11:27:41 -0800

    add explanatory comments to sample dem creation script

--- scripts/create_test_dem.sh
+++ scripts/create_test_dem.sh
@@ -1,11 +1,13 @@
 # Example of creating a DEM from another source
 
-# Note that the data sources listed in tdem_list.txt are
-# internal to bcgov.
-
+# Create a vrt from multiple source files
+# *Note* files listed in tdem_list.txt are internal to bcgov
 gdalbuildvrt gdalbuildvrt dem_index.vrt \
   -input_file_list tdem_list.txt
 
+# Mosaic the files listed in the vrt into a single file.
+# As source files are already BC Albers, we just need to specify the exact
+# output extent to get things to line up correctly
 gdalwarp dem_index.vrt dem_robson.tif \
   -te 1318987.5 807387.5 1536487.5 1006587.5 \
   -r bilinear \
